bf72a126c31f18a9190fa62ca13a9ec96466c574
“Sean” <seam.b22@gmail.com>
2022-04-24 14:35:03 -0600

    pulled routers

--- src/components/products.js
+++ src/components/products.js
@@ -1,1 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { callApi } from "../api";
+
+const Products = ({ products, setProducts }) => {
+  useEffect(() => {
+    const getProducts = async () => {
+      const data = await callApi({
+        url: `/products`,
+        method: "GET",
+      });
+      setProducts(data);
+    };
+    getProducts();
+  }, []);
+
+  return (
+    <>
+      <h1>Meals-And-Deals Product Listings</h1>
+      {products.map((product) => {
+        return (
+          <div className="singleProduct" key={product.id}>
+            <h2> Product: {product.name}</h2>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default Products;
