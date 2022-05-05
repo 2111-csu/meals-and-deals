0ff8b6aebd77060a6ad33a9a7f297b5c5ee2d7f3
L Ludwin <ludwinlaura@hotmail.com>
2022-05-05 10:51:09 -0600

    orderproducts api, index router, getOrderById, init_db test

--- api/index.js
+++ api/index.js
@@ -1,11 +1,11 @@
-const apiRouter = require("express").Router();
+const router = require("express").Router();
 const jwt = require('jsonwebtoken');
 const { getUserById } = require('../db');
 const client = require('../db/client');
 const { JWT_SECRET = 'soSecret' } = process.env;
 
 
-apiRouter.get("/health", (req, res, next) => {
+router.get("/health", (req, res, next) => {
   res.send({
     healthy: true,
   });
@@ -16,7 +16,7 @@ apiRouter.get("/health", (req, res, next) => {
 
 // set `req.user` if possible
 
-apiRouter.use(async (req, res, next) => {
+router.use(async (req, res, next) => {
   const prefix = 'Bearer ';
   const auth = req.header('Authorization');
   
@@ -30,6 +30,7 @@ apiRouter.use(async (req, res, next) => {
       
       const id = parsedToken && parsedToken.id
       if (id) {
+        console.log("req USER!!!", req.user);
         req.user = await getUserById(id);
         next();
       }
@@ -44,29 +45,29 @@ apiRouter.use(async (req, res, next) => {
   }
 });
 
-apiRouter.use((req, res, next) => {
+router.use((req, res, next) => {
   if (req.user) {
     console.log("User is set:", req.user);
   }
   next();
 });
 
-apiRouter.get("/", (req, res, next) => {
+router.get("/", (req, res, next) => {
   res.send({
     message: "API is under construction!",
   });
 });
 
-const orderProductsRouter = require('./orderProducts');
- apiRouter.use('/order_products', orderProductsRouter);
+const orderProductsRouter = require('./order_products');
+ router.use('/order_products', orderProductsRouter);
 
 const productsRouter = require('./products');
-apiRouter.use('/products', productsRouter);
+router.use('/products', productsRouter);
 
 const ordersRouter = require('./orders');
-apiRouter.use('/orders', ordersRouter);
+router.use('/orders', ordersRouter);
 
 const usersRouter = require('./users');
-apiRouter.use('/users', usersRouter);
+router.use('/users', usersRouter);
 
-module.exports = apiRouter;
+module.exports = router;
--- api/order_products.js
+++ api/order_products.js
@@ -3,8 +3,58 @@ const router = express.Router();
 const jwt = require('jsonwebtoken');
 const { requireUser } = require("./utils");
 const client = require('../db/client');
+const { updateOrderProduct, getOrderById, destroyOrderProduct} = require("../db");
 const { JWT_SECRET = 'soSecret' } = process.env;
 
 
+router.patch('/:orderProductId', requireUser, async (req, res, next) =>{
+    
+    const {quantity, price} = req.body;
+    const {orderProductId} = req.params;
+    const {id} = req.user;
+    const fields = {};
+    
+    if(quantity) {
+        fields.quantity = quantity;
+    }
+
+    if (price) {
+        fields.price = price;
+    }
+
+    try{
+        const product = await updateOrderProduct({
+            id: orderProductId,
+            ...fields,
+        });
+        const order = await getOrderById(product.orderId);
+        
+            res.send(product);
+        
+    } catch(error) {
+        next(error);
+    }  
+});
+
+router.delete('/:orderProductId', requireUser, async (req, res, next) => {
+    const { orderProductId } = req.params;
+
+    try {
+        const opId = await getOrderProductById(orderProductId);
+        const userInfo = await getOrderById(opId.orderId);
+        
+        if (opId && userInfo.creatorId === req.user.id) {
+            const product = await destroyOrderProduct(orderProductId);
+            res.send(product);
+        } else {
+            next({
+                name: 'OrderIDNotFoundError',
+                message: 'That order ID does not exist'
+            });
+        }
+    } catch (error) {
+        next (error);
+    }
+});
 
 module.exports = router;
