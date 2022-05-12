70e49827fde947723f7232ce449b2a57da89b742
L Ludwin <ludwinlaura@hotmail.com>
2022-05-12 16:00:54 -0600

    addProduct2

--- src/components/AddProduct.js
+++ src/components/AddProduct.js
@@ -14,7 +14,37 @@ const AddProduct = ({ token, setProducts }) => {
         event.preventDefault();
 
         await callApi({url: '/products', method: 'POST', token, body: { name, description, price, imageURL, inStock, category} });
+        const productsResponse = await callApi({url: `/products`, method: "GET"})
 
-    }}
+        setProducts(productsResponse);
+        setName('');
+        setDescription('');
+        setPrice('');
+        setImageURL('');
+        setInStock('');
+        setCategory('');
+    };
+
+    const reRenderProducts = async () => {
+    const allProducts = await callApi({url:`/products`, method: "GET"});
+    setProducts(productsResponse);
+    reRenderProducts();
+};
+
+return <>
+<br/>
+    <h2>Create a new Product for Meals and Deals</h2>
+    <br/>
+     <form onSubmit={handleSubmit} >
+            <input type='text' placeholder='New Product Name' value={name} onChange={(event) => setName(event.target.value)} />
+            <input type='text' placeholder='New Product Description' value={description} onChange={(event) => setDescription(event.target.value)} />
+            <input type='text' placeholder='New Product Price' value={price} onChange={(event) => setPrice(event.target.value)} />
+            <input type='text' placeholder='New Product ImageURL' value={imageURL} onChange={(event) => setImageURL(event.target.value)} />
+            <input type='text' placeholder='New Product InStock' value={inStock} onChange={(event) => setInStock(event.target.value)} />
+            <input type='text' placeholder='New Product Category' value={category} onChange={(event) => setCategory(event.target.value)} />
+            <button type="submit">Submit</button>
+    </form>
+    </>
+}
 
 export default AddProduct;
