5cc2a0d01be7d49389be2214dea356b60f13cbe7
“Sean” <seam.b22@gmail.com>
2022-05-14 11:30:23 -0600

    admin user form

--- api/orders.js
+++ api/orders.js
@@ -1,40 +1,40 @@
 const express = require("express");
 const router = express.Router();
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const { requireUser } = require("./utils");
-const client = require('../db/client');
-const { JWT_SECRET = 'soSecret' } = process.env;
+const client = require("../db/client");
+const { JWT_SECRET = "soSecret" } = process.env;
 
 const {
-    getAllOrders,
-    createOrders,
-    getAllUsers,
-    getUserById,
-    getOrdersByUser,
-    getUserByUsername,
-    getCartByUser,
-    getOrderById,
-    addProductToOrder,
-    updateOrderProduct,
-    completeOrder,
-    cancelOrder
-  } = require("../db");
-  
-// GET /orders (*admin)  
-router.get ("/", async (req, res, next) => {
-    try{
-        const orders = await getAllOrders();
-        //need to add isAdmin for user
-        res.send(orders);
-    }   catch (error) {
-        next(error);
-    }
+  getAllOrders,
+  createOrders,
+  getAllUsers,
+  getUserById,
+  getOrdersByUser,
+  getUserByUsername,
+  getCartByUser,
+  getOrderById,
+  addProductToOrder,
+  updateOrderProduct,
+  completeOrder,
+  cancelOrder,
+} = require("../db");
+
+// GET /orders (*admin)
+router.get("/", async (req, res, next) => {
+  try {
+    const orders = await getAllOrders();
+    //need to add isAdmin for user
+    res.send(orders);
+  } catch (error) {
+    next(error);
+  }
 });
-  
+
 // /api/orders
 
 router.get("/cart", requireUser, async (req, res, next) => {
-  const {id} = req.user
+  const { id } = req.user;
   try {
     const cart = await getCartByUser(id);
     res.send(cart);
@@ -43,54 +43,60 @@ router.get("/cart", requireUser, async (req, res, next) => {
   }
 });
 
-  
-  //  POST /orders (*)
-  // Create a new order. Should initially be status = created.
+//  POST /orders (*)
+// Create a new order. Should initially be status = created.
 router.post("/", async (req, res, next) => {
-    try {
-      const { status, userId, datePlaced, creatorName, products } = req.body;
-      const order = await createOrders({
-        status,
-        userId,
-        datePlaced,
-        creatorName,
-        products,
-      });
-      res.send(order);
-    } catch (error) {
-      next(error);
-    }
-  });
+  try {
+    const { status, userId, datePlaced, creatorName, products } = req.body;
+    const order = await createOrders({
+      status,
+      userId,
+      datePlaced,
+      creatorName,
+      products,
+    });
+    res.send(order);
+  } catch (error) {
+    next(error);
+  }
+});
 
-
-router.get('/:userId/orders',  async (req, res, next) => {
-
-try{
-    const {userId}= req.params;
+router.get("/:userId/orders", async (req, res, next) => {
+  try {
+    const { userId } = req.params;
     const user = await getUserByUsername(userId);
     // the other db function to try would be getUserById(userId)
     const order = await getCartByUser(user);
     res.send(order);
-}catch(error){
+  } catch (error) {
     next(error);
-}
+  }
 });
 
-router.post('/:orderId/products', async (req, res, next) => {
+router.post("/:orderId/products", async (req, res, next) => {
   const orderId = req.params.orderId;
-  const {productId, price, quantity} = req.body;
-  try{
-    const allOrderProducts =  await getOrderById(orderId);
-    const orderProducts = allOrderProducts[0].products
-    const filteredOP = orderProducts.filter(product => product.productId == productId);
-    
-    if(filteredOP.length > 0){
+  const { productId, price, quantity } = req.body;
+  try {
+    const allOrderProducts = await getOrderById(orderId);
+    const orderProducts = allOrderProducts[0].products;
+    const filteredOP = orderProducts.filter(
+      (product) => product.productId == productId
+    );
+
+    if (filteredOP.length > 0) {
       const newQuantity = filteredOP.quantity + quantity;
-      const updateQuantity = await updateOrderProduct({id : filteredOP.id , quantity : newQuantity})
-      res.send(updateQuantity)
-      
-    }else{
-      const product = await addProductToOrder({orderId, productId, price, quantity});
+      const updateQuantity = await updateOrderProduct({
+        id: filteredOP.id,
+        quantity: newQuantity,
+      });
+      res.send(updateQuantity);
+    } else {
+      const product = await addProductToOrder({
+        orderId,
+        productId,
+        price,
+        quantity,
+      });
       res.send(product);
     }
   } catch (error) {
@@ -98,24 +104,24 @@ router.post('/:orderId/products', async (req, res, next) => {
   }
 });
 
-router.post('/:orderId', requireUser, async (req, res, next) => {
+router.post("/:orderId", requireUser, async (req, res, next) => {
   try {
-    const { orderId } = req.params
-    const order = await completeOrder(orderId)
-  res.send(order)
-  }catch (error) {
+    const { orderId } = req.params;
+    const order = await completeOrder(orderId);
+    res.send(order);
+  } catch (error) {
     next(error);
   }
-})
+});
 
-router.delete('/:orderId', requireUser, async (req, res, next) => {
+router.delete("/:orderId", requireUser, async (req, res, next) => {
   try {
-    const { orderId } = req.params
-    const order = await cancelOrder(orderId)
-  res.send(order)
-  }catch (error) {
+    const { orderId } = req.params;
+    const order = await cancelOrder(orderId);
+    res.send(order);
+  } catch (error) {
     next(error);
   }
-})
+});
 
 module.exports = router;
