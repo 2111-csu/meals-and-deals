d390732f258ac934ef219d2f2805b9e39151d69a
bourquinms <bourquinm@aol.com>
2022-05-14 10:18:17 -0600

    changed products.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 export { default as App } from './App';
-export { default as Products } from './Products'
+export { default as Products } from './ProductsList'
 export { default as SingleProduct } from './SingleProduct'
 export { default as RegisterLogin } from './RegisterLogin'
 export { default as Home } from './Home'
