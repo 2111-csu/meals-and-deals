fc2a68701e99bfcec55fde86708825701e877f56
L Ludwin <ludwinlaura@hotmail.com>
2022-05-16 16:05:35 -0600

    css8

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,8 +8,9 @@ const Home = ({userName, message}) => {
             <br/>
         <img src="images/home.png" className="welcome" alt="welcome to meals-and-deals"/>
         <br/>
-        
+        <img src="images/works.png" className="works" alt="meals-and-deals details"/>
         <br/>
+        <img src="images/homepage2.png" className="welcome" alt="image of pasta"/>
         <br/>
         {(userName ==='') ? <h2>You are not Signed in</h2> : <h2>You are Signed in as {userName}</h2>}
         </div>
